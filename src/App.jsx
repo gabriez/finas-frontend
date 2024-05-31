1639d8d74b2b2a37408661316c843ddd5bed0b1c
gabotdev <gabrieltorres2014@gmail.com>
2024-05-31 00:17:21 -0400

    explicacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+let ventas = [
+  {
+    precio: 5,
+    nombre: 'silla 1'
+  },
+  {
+    precio: 5,
+    nombre: 'silla 2'
+  },
+  {
+    precio: 5,
+    nombre: 'silla 3'
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function handleClick (e) {
+    setCount((prevState) => {
+      return prevState + 1
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Cuenta: {count} </p>
+      <button onClick={handleClick}> cuenta </button>
+      {
+        
+      }
     </>
   )
 }
