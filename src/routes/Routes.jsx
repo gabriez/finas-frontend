de537521975d704168e362d72c002b4a6d342512
gabotdev <gabrieltorres2014@gmail.com>
2024-10-04 12:28:42 -0400

    main | adding private routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,41 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import Layout from '../layouts/Layout'
-import Login from '../pages/public/Login'
-import Register from '../pages/public/Register';
-import Confirm from '../pages/public/Confirm';
+import Layout from "../layouts/Layout";
+import Login from "../pages/public/Login";
+import Register from "../pages/public/Register";
+import Confirm from "../pages/public/Confirm";
+import PrivateLayout from "../layouts/PrivateLayout";
+import Home from "../pages/private/Home";
 
 const routes = {
-    PUBLIC: {
-        INDEX: '/',
-        REGISTER: '/register',
-        CONFIRM: '/confirmacion',
-        FORGOT: '/forgot-password'
-    }
-}
+	PUBLIC: {
+		INDEX: "/",
+		REGISTER: "/register",
+		CONFIRM: "/confirmacion",
+		FORGOT: "/forgot-password",
+	},
+};
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                index: true, 
-                element: <Login/>
-            }, 
-            {
-                path: "/register",
-                element: <Register />,
-            }, 
-            {
-                path: "/confirmacion",
-                element: <Confirm />
-            }
-        ]
-    }
-])
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <Login />,
+			},
+			{
+				path: "/register",
+				element: <Register />,
+			},
+			{
+				path: "/confirmacion",
+				element: <Confirm />,
+			},
+		],
+	},
+	{
+		path: "/dashboard",
+		element: <PrivateLayout />,
+		children: [{ index: true, element: <Home /> }],
+	},
+]);
 
 export default router;
-export {
-    routes
-};
+export { routes };
