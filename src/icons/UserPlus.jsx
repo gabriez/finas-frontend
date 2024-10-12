ddafd2dd2397b5e4e699f6a39d44a66102881ec5
Leonardo Linares <leonardolinaresteran@gmail.com>
2024-10-12 14:21:33 -0400

    users

---
+++ src/icons/UserPlus.jsx
@@ -0,0 +1,20 @@
+const UserPlus = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={40}
+    height={40}
+    fill="none"
+    {...props}
+  >
+    <g
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <path d="M18.333 5.583A9.667 9.667 0 0 1 21.667 5a10 10 0 1 1-9.417 13.333" />
+      <path d="M15.133 22.733a11.667 11.667 0 0 0-6.8 10.6A1.667 1.667 0 0 0 10 35h23.333A1.666 1.666 0 0 0 35 33.333a11.666 11.666 0 0 0-6.5-10.45M11.667 8.333H5M8.333 5v6.667" />
+    </g>
+  </svg>
+)
+export default UserPlus;
--- src/pages/private/Usuarios.jsx
+++ src/pages/private/Usuarios.jsx
@@ -1,8 +1,12 @@
+import UserPlus from "../../icons/UserPlus"
 
 const Usuarios = () => {
   return (
     <div className="text-white flex flex-col justify-center min-h-screen items-center">
-      pene
+      <div className="flex 2xl:w-[1600px] w-[1200px] justify-between items-center" style={{color: '#ffffff', fontFamily: 'Poppins-Black' }}>
+        <h1 className="">Lista de Usuarios</h1>
+        <button><UserPlus className=""/> Añadir Usuario</button> 
+      </div>
     </div>
   )
 }
