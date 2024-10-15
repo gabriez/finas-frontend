ceb302f6ed68f3cfdbd246da4acc23ca22bbd4fc
Leonardo Linares <leonardolinaresteran@gmail.com>
2024-10-15 19:13:45 -0400

    text

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -8,6 +8,9 @@ import { IoCreateOutline } from "react-icons/io5";
 import { TiDelete } from "react-icons/ti";
 import { FINASAPI } from '../../lib/FinasApi';
 import { toast } from 'react-toastify';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 // const users = [
 //   { name: "The Sliding", email: "ejemplo@gmail.com", username: "username.example12", role: "Administrador" },
@@ -39,6 +42,26 @@ const UserList = () => {
 
   const handleModalClose = () => {
     setShowModal(false);
+  };
+
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    rol: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
   };
 
   console.log("users > ", users)
@@ -84,9 +107,104 @@ const UserList = () => {
             </tr>
         </Table>
         <Pagination/>
-        <CustomModal className="w-[579px] h-[753px] bg-white rounded-[30px] shadow overflow-hidden" show={showModal} onClose={handleModalClose}>
-        <h2 className="w-[579px] p-9 bg-[#073d0b] rounded-tl-[15px] rounded-tr-[30px] shadow text-white text-4xl font-bold font-['Poppins'] text-center">Editar Usuario</h2>
-        {/* Aquí puedes añadir un formulario o cualquier otro contenido */}
+        <CustomModal
+          className="w-[579px] h-[753px] bg-white rounded-[30px] shadow overflow-hidden"
+          show={showModal}
+          onClose={handleModalClose}
+        >
+
+        <h2
+          className="w-[579px] p-9 bg-[#073d0b] rounded-tl-[15px] rounded-tr-[30px] shadow text-white text-4xl font-bold font-['Poppins'] text-center">
+          Editar Usuario
+        </h2>
+
+        <form
+        onSubmit={handleSubmit}
+        className=" flex flex-wrap gap-5 items-center content-center justify-center w-[100%] bg-white p-8  rounded-lg "
+        >
+        
+        <div className="w-[410px] h-20 flex-col justify-start items-start gap-2.5 inline-flex">
+
+        <label 
+          className="block text-gray-700 text-lg font-bold mt-2"
+          htmlFor="name"
+        >
+          Nombre
+        </label>
+
+          <input
+            type="text"
+            name="name"
+            placeholder="Introduzca su nombre"
+            value={formData.name}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        
+
+        <label 
+          className="block text-gray-700 text-lg font-bold mt-2"
+          htmlFor="lastName"
+        >
+          Apellido
+        </label>
+
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Introduzca su apellido"
+            value={formData.lastName}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+
+        <label
+        className="block text-gray-700 text-lg font-bold mt-2"
+        htmlFor="email"
+        >
+          Email:
+        </label>
+
+          <input
+            type="email"
+            name="email"
+            placeholder="ejemplo@hotmail.com"
+            value={formData.email}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+
+        <label 
+        className="block text-gray-700 text-lg font-bold mt-2"
+        htmlFor="rol"
+        >
+          Rol:
+          <select
+            name="rol"
+            id="rol"
+            placeholder="Seleciona un rol"
+            value={formData.rol}
+            onChange={handleChange}
+            className="shadow  border rounded w-full h-[50px] py-2 px-8 text-gray-700 inline-flex"
+          > 
+          <option value="" className="text-[#a3a9b2] text-xl font-normal font-['Inter'] leading-relaxed" disabled>Seleccione el rol del usuario</option>
+            <option value="usuario">Usuario</option>
+            <option value="supervisor">Supervisor</option>
+            <option value="administrador">Administrador</option>
+            </select>
+        </label>
+
+          <button
+            type="submit"
+            className=" bg-[#3CAC38] hover:bg-[#063A0A] mx-[8rem] my-[2rem] text-white font-bold text-xl py-2 px-10 rounded focus:outline-none focus:shadow-outline hover:shadow-2xl"
+          >
+            Guardar
+          </button>
+
+        </div>
+      </form>
+        
+
       </CustomModal>
     </div>  
   )
