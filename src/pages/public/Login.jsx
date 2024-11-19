0ea76eb12df525ec1defa3b4efef3c1ec2b06b40
derekrojo <derekrojo@gmail.com>
2024-11-19 01:04:55 -0400

    validaciones login

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -8,82 +8,111 @@ import { LOCAL_STORAGE_KEYS, TITLE } from "../../constants";
 import { toast } from "react-toastify";
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../../context/AuthProvider";
+import * as Yup from "yup";
+
+const schemaLogin = Yup.object({
+  email: Yup.string().email().required("Campo requerido"),
+  password: Yup.string()
+    .matches(
+      /^[a-zA-Z0-9]{3,30}$/,
+      "La contraseña necesita como mínimo 3 caracteres"
+    )
+    .required("Campo requerido"),
+});
 
 const Login = () => {
-	const [formData, setFormData] = useState({
-		email: "",
-		password: "",
-	});
-	document.title = `${TITLE} - Inicio de sesión`;
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  document.title = `${TITLE} - Inicio de sesión`;
 
-	const { setUserData } = useAuth();
+  const { setUserData } = useAuth();
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prevState) => {
-			return { ...prevState, [name]: value };
-		});
-	};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			let result = await FINASAPI.login(formData.email, formData.password);
-			if (result.status) {
-				let jwt = jwtDecode(result.data.token);
-				let exp = jwt.exp;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await schemaLogin.validate(formData, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const validationErrors = {};
+      err.inner.forEach((error) => {
+        if (error.path && !validationErrors[error.path]) {
+          validationErrors[error.path] = error.message;
+        }
+      });
+      setErrors(validationErrors);
 
-				sessionStorage.setItem(
-					LOCAL_STORAGE_KEYS.accessToken,
-					result.data.token
-				);
-				setUserData({
-					username: result.data.user.username,
-					email: result.data.user.email,
-					rol: result.data.user.rol,
-					endTime: exp,
-					id: result.data.user.id,
-				});
-				toast.success("Inició sesión exitosamente");
-				navigate(ROUTES.PRIVATE.INDEX);
-			} else {
-				console.log(result.data);
-				toast.error(result.message);
-			}
-		} catch (error) {
-			console.log(error);
-			toast.error("Ocurrio un error en el inicio de sesión");
-		}
-	};
+      return;
+    }
+    try {
+      let result = await FINASAPI.login(formData.email, formData.password);
+      if (result.status) {
+        let jwt = jwtDecode(result.data.token);
+        let exp = jwt.exp;
 
-	return (
-		<>
-			<form className="flex-col" onSubmit={handleSubmit}>
-				<div className="pb-2">
-					<InputText
-						type="email"
-						name="email"
-						placeholder="Email"
-						handleChange={handleChange}
-						value={formData.email}
-					/>
-				</div>
-				<div className="pb-5">
-					{/* <label htmlFor="password" className="text-"></label> */}
-					<InputText
-						type="password"
-						name="password"
-						placeholder="Contraseña"
-						handleChange={handleChange}
-						value={formData.password}
-					/>
-				</div>
-				<ButtonSubmit text="Iniciar Sesion" />
-			</form>
-		</>
-	);
+        sessionStorage.setItem(
+          LOCAL_STORAGE_KEYS.accessToken,
+          result.data.token
+        );
+        setUserData({
+          username: result.data.user.username,
+          email: result.data.user.email,
+          rol: result.data.user.rol,
+          endTime: exp,
+          id: result.data.user.id,
+        });
+        toast.success("Inició sesión exitosamente");
+        navigate(ROUTES.PRIVATE.INDEX);
+      } else {
+        console.log(result.data);
+        toast.error(result.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Ocurrio un error en el inicio de sesión");
+    }
+  };
+
+  return (
+    <>
+      <form className="flex-col" onSubmit={handleSubmit}>
+        <div className="pb-2">
+          <InputText
+            type="email"
+            name="email"
+            placeholder="Email"
+            handleChange={handleChange}
+            value={formData.email}
+            error={errors.email}
+          />
+        </div>
+        <div className="pb-2">
+          {/* <label htmlFor="password" className="text-"></label> */}
+          <InputText
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            handleChange={handleChange}
+            value={formData.password}
+            error={errors.password}
+          />
+        </div>
+        <ButtonSubmit text="Iniciar Sesion" />
+      </form>
+    </>
+  );
 };
 
 export default Login;
